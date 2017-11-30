2e31120ac9850b4bb4ae286f438d5aa6335b5eaf
mosouley <mo.souley@gmail.com>
2017-11-30 14:52:37 +0100

    pilierController en local

--- src/main/java/com/bootcamp/controllers/PilierController.java
+++ src/main/java/com/bootcamp/controllers/PilierController.java
@@ -1,7 +1,6 @@
 package com.bootcamp.controllers;
 
 import com.bootcamp.commons.exceptions.DatabaseException;
-import com.bootcamp.commons.ws.models.Error;
 import com.bootcamp.commons.ws.models.PilierWs;
 import com.bootcamp.commons.ws.models.PilierWss;
 import com.bootcamp.entities.Pilier;
@@ -9,7 +8,6 @@ import com.bootcamp.services.PilierService;
 import com.bootcamp.version.ApiVersions;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.logging.log4j.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,117 +33,78 @@ public class PilierController {
     @RequestMapping(method = RequestMethod.POST, value = "/")
     @ApiVersions({"1.0"})
     @ApiOperation(value = "Create a new pilier", notes = "Create a new pilier")
-    public ResponseEntity<PilierWs> create(@RequestBody @Valid Pilier pilier) {
+    public ResponseEntity<PilierWs> create(@RequestBody @Valid Pilier pilier) throws SQLException {
 
         PilierWs pilierWs = new PilierWs();
         HttpStatus httpStatus = null;
 
-        try {
             pilierService.create(pilier);
             pilierWs.setData(pilier);
             httpStatus = HttpStatus.OK;
-        }catch (SQLException exception){
-            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-            Error error = new Error();
-            error.setMessage(errorMessage);
-            pilierWs.setError(error);
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
 
-        return new ResponseEntity<PilierWs>(pilierWs, httpStatus);
+        return new ResponseEntity<>(pilierWs, httpStatus);
     }
 
 
     @RequestMapping(method = RequestMethod.PUT, value = "/")
     @ApiVersions({"1.0"})
     @ApiOperation(value = "Update a new pilier", notes = "Update a new pilier")
-    public ResponseEntity<PilierWs> update(@RequestBody @Valid Pilier pilier) {
+    public ResponseEntity<PilierWs> update(@RequestBody @Valid Pilier pilier) throws SQLException {
 
         PilierWs pilierWs = new PilierWs();
         HttpStatus httpStatus = null;
 
-        try {
             pilierService.update(pilier);
             pilierWs.setData(pilier);
             httpStatus = HttpStatus.OK;
-        }catch (SQLException exception){
-            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-            Error error = new Error();
-            error.setMessage(errorMessage);
-            pilierWs.setError(error);
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
+       
 
-        return new ResponseEntity<PilierWs>(pilierWs, httpStatus);
+        return new ResponseEntity<>(pilierWs, httpStatus);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     @ApiVersions({"1.0"})
     @ApiOperation(value = "Delete a pilier", notes = "Delete a pilier")
-    public ResponseEntity<PilierWs> delete(@PathVariable(name = "id") int id) {
+    public ResponseEntity<PilierWs> delete(@PathVariable(name = "id") int id) throws SQLException {
 
         PilierWs pilierWs = new PilierWs();
         HttpStatus httpStatus = null;
 
-        try {
             Pilier pilier = pilierService.delete(id);
             pilierWs.setData(pilier);
             httpStatus = HttpStatus.OK;
-        }catch (SQLException exception){
-            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-            Error error = new Error();
-            error.setMessage(errorMessage);
-            pilierWs.setError(error);
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
 
-        return new ResponseEntity<PilierWs>(pilierWs, httpStatus);
+        return new ResponseEntity<>(pilierWs, httpStatus);
     }
 
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     @ApiVersions({"1.0"})
     @ApiOperation(value = "Read a pilier", notes = "Read a pilier")
-    public ResponseEntity<PilierWs> read(@PathVariable(name = "id") int id) {
+    public ResponseEntity<PilierWs> read(@PathVariable(name = "id") int id) throws SQLException {
 
         PilierWs pilierWs = new PilierWs();
         HttpStatus httpStatus = null;
 
-        try {
             Pilier pilier = pilierService.read(id);
             pilierWs.setData(pilier);
             httpStatus = HttpStatus.OK;
-        }catch (SQLException exception){
-            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-            Error error = new Error();
-            error.setMessage(errorMessage);
-            pilierWs.setError(error);
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
 
-        return new ResponseEntity<PilierWs>(pilierWs, httpStatus);
+        return new ResponseEntity<>(pilierWs, httpStatus);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
     @ApiVersions({"1.0"})
     @ApiOperation(value = "Read a pilier", notes = "Read a pilier")
-    public ResponseEntity<PilierWss> read() {
+    public ResponseEntity<PilierWss> read() throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
 
         PilierWss pilierWss = new PilierWss();
         HttpStatus httpStatus = null;
 
-        try {
             List<Pilier> piliers = pilierService.read(request);
             pilierWss.setData(piliers);
             httpStatus = HttpStatus.OK;
-        }catch (SQLException | IllegalAccessException | DatabaseException | InvocationTargetException exception){
-            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-            Error error = new Error();
-            error.setMessage(errorMessage);
-            pilierWss.setError(error);
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
 
-        return new ResponseEntity<PilierWss>(pilierWss, httpStatus);
+        return new ResponseEntity<>(pilierWss, httpStatus);
     }
 }
