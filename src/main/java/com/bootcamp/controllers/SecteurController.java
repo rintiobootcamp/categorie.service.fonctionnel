40d9774267ba0d3038c5e1bb4162851aaed6edb2
ibrahim00017 <hibrahimyya@gmail.com>
2017-12-02 18:34:06 +0100

    after testing

--- src/main/java/com/bootcamp/controllers/SecteurController.java
+++ src/main/java/com/bootcamp/controllers/SecteurController.java
@@ -1,14 +1,52 @@
 package com.bootcamp.controllers;
 
-//@RequestMapping("/secteur")
-//@Api(value = "Secteur API", description = "Secteur API")
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.entities.Secteur;
+import com.bootcamp.services.SecteurService;
+import com.bootcamp.version.ApiVersions;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/secteurs")
+@RestController("SecteurContoller")
+@Api(value = "Secteur API", description = "Secteur API")
 public class SecteurController {
 //
-//    @Autowired
-//    SecteurService secteurService;
-//
-//    @Autowired
-//    HttpServletRequest request;
+    @Autowired
+    SecteurService secteurService;
+
+
+    @Autowired
+    HttpServletRequest request;
+
+        @RequestMapping(method = RequestMethod.GET)
+        @ApiVersions({"1.0"})
+        @ApiOperation(value = "Read All Secteurs", notes = "Read aall the Secteurs")
+        public ResponseEntity<List<Secteur>> read()throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+            List<Secteur> secteurs=secteurService.read(request);
+            return new ResponseEntity<List<Secteur>>(secteurs, HttpStatus.OK);
+        }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Get one Secteurs", notes = "Read a particular Secteurs")
+    public ResponseEntity<Secteur> getById(@PathVariable int id)throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+        Secteur secteur=secteurService.read(id);
+        return new ResponseEntity<Secteur>(secteur, HttpStatus.OK);
+    }
 //
 //    @RequestMapping(method = RequestMethod.POST,value = "/")
 //    @ApiVersions({"1.0"})
@@ -103,24 +141,6 @@ public class SecteurController {
 //
 //        return new ResponseEntity<SecteurWs>(SecteurWs, httpStatus);
 //    }
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Read a Secteur", notes = "Read a Secteur")
-//    public ResponseEntity<List<Secteur>> read() {
-//        List<Secteur> secteurs=new ArrayList<Secteur>();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//             secteurs = secteurService.read(request);
-//
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException | IllegalAccessException | DatabaseException | InvocationTargetException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<List<Secteur>>(secteurs, httpStatus);
-//    }
+
+
 }
