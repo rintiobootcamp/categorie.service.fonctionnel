07772a25679e2d8f899b629c1e7757bb43b2ae5a
tdarex <tdarex@asklytics.io>
2017-12-02 05:33:15 +0100

    Implement controller for axe and secteur

--- src/main/java/com/bootcamp/controllers/SecteurController.java
+++ src/main/java/com/bootcamp/controllers/SecteurController.java
@@ -1,126 +1,53 @@
 package com.bootcamp.controllers;
 
-//@RequestMapping("/secteur")
-//@Api(value = "Secteur API", description = "Secteur API")
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.entities.Secteur;
+import com.bootcamp.services.SecteurService;
+import com.bootcamp.version.ApiVersions;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/secteur")
+@Api(value = "Secteur API", description = "Secteur API")
 public class SecteurController {
-//
-//    @Autowired
-//    SecteurService secteurService;
-//
-//    @Autowired
-//    HttpServletRequest request;
-//
-//    @RequestMapping(method = RequestMethod.POST,value = "/")
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Create a new Secteur", notes = "Create a new Secteur")
-//    public ResponseEntity<SecteurWs> create(@RequestBody @Valid Secteur Secteur) {
-//
-//        SecteurWs SecteurWs = new SecteurWs();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//            secteurService.create(Secteur);
-//            SecteurWs.setData(Secteur);
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//            Error error = new Error();
-//            error.setMessage(errorMessage);
-//            SecteurWs.setError(error);
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<SecteurWs>(SecteurWs, httpStatus);
-//    }
-//
-//
-//    @RequestMapping(method = RequestMethod.PUT, value = "/")
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Update a new Secteur", notes = "Update a new Secteur")
-//    public ResponseEntity<SecteurWs> update(@RequestBody @Valid Secteur Secteur) {
-//
-//        SecteurWs SecteurWs = new SecteurWs();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//            secteurService.update(Secteur);
-//            SecteurWs.setData(Secteur);
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//            Error error = new Error();
-//            error.setMessage(errorMessage);
-//            SecteurWs.setError(error);
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<SecteurWs>(SecteurWs, httpStatus);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Delete a Secteur", notes = "Delete a Secteur")
-//    public ResponseEntity<SecteurWs> delete(@PathVariable(name = "id") int id) {
-//
-//        SecteurWs SecteurWs = new SecteurWs();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//            Secteur Secteur = secteurService.delete(id);
-//            SecteurWs.setData(Secteur);
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//            Error error = new Error();
-//            error.setMessage(errorMessage);
-//            SecteurWs.setError(error);
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<SecteurWs>(SecteurWs, httpStatus);
-//    }
-//
-//
-//    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Read a Secteur", notes = "Read a Secteur")
-//    public ResponseEntity<SecteurWs> read(@PathVariable(name = "id") int id) {
-//
-//        SecteurWs SecteurWs = new SecteurWs();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//            Secteur Secteur = secteurService.read(id);
-//            SecteurWs.setData(Secteur);
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//            Error error = new Error();
-//            error.setMessage(errorMessage);
-//            SecteurWs.setError(error);
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<SecteurWs>(SecteurWs, httpStatus);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    @ApiVersions({"1.0"})
-//    @ApiOperation(value = "Read a Secteur", notes = "Read a Secteur")
-//    public ResponseEntity<List<Secteur>> read() {
-//        List<Secteur> secteurs=new ArrayList<Secteur>();
-//        HttpStatus httpStatus = null;
-//
-//        try {
-//             secteurs = secteurService.read(request);
-//
-//            httpStatus = HttpStatus.OK;
-//        }catch (SQLException | IllegalAccessException | DatabaseException | InvocationTargetException exception){
-//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
-//
-//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-//        }
-//
-//        return new ResponseEntity<List<Secteur>>(secteurs, httpStatus);
-//    }
+
+    @Autowired
+    SecteurService secteurService;
+
+    @Autowired
+    HttpServletRequest request;
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Read a Secteur", notes = "Read a Secteur")
+    public ResponseEntity<Secteur> read(@PathVariable(name = "id") int id) throws SQLException {
+
+        Secteur secteur = secteurService.read(id);
+        return new ResponseEntity<Secteur>(secteur, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Read a Secteur", notes = "Read a Secteur")
+    public ResponseEntity<List<Secteur>> read() throws InvocationTargetException, SQLException, DatabaseException, IllegalAccessException {
+        List<Secteur> secteurs = new ArrayList<>();
+        HttpStatus httpStatus = null;
+
+        secteurs = secteurService.read(request);
+
+        return new ResponseEntity<List<Secteur>>(secteurs, HttpStatus.OK);
+    }
 }
