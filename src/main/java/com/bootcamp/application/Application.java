7517d10dfe96d6f1db757ab2331a0b7a89465653
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-24 17:48:20 +0100

    elast

--- src/main/java/com/bootcamp/application/Application.java
+++ src/main/java/com/bootcamp/application/Application.java
@@ -17,7 +17,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
